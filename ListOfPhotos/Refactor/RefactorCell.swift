515452aab8a43cba5d038c853c55f29142f977d5
Misha <Mishka.ganin@mail.ru>
2024-12-05 20:19:45 +0300

    add list

---
+++ ListOfPhotos/Refactor/RefactorCell.swift
@@ -0,0 +1,50 @@
+//
+//  RefactorCell.swift
+//  ListOfPhotos
+//
+//  Created by Михаил Ганин on 05.12.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+struct RefactorCell: View {
+    @ObservedObject var photo: PhotoResponseModel
+    @Environment(\.modelContext) private var modelContext
+    @Environment(\.editMode) var editMode
+    @State private var isEditing = false
+
+
+    init(photo: PhotoResponseModel) {
+        self.photo = photo
+    }
+    
+    var body: some View {
+        Form {
+            Section(header: Text("detail")) {
+                if editMode?.wrappedValue == .active {
+                    TextField("todo", text: $photo.title)
+                        .font(.title)
+                        .onChange(of: photo.title) { newValue in
+                            isEditing = true
+                        }
+                        .onSubmit {
+                            if isEditing {
+                                isEditing = false
+                            }
+                        }
+                } else {
+                    Text(photo.title)
+                        .font(.title)
+                }
+            }
+        }
+        .toolbar {
+            EditButton()
+        }
+    }
+}
+
+
+
+
